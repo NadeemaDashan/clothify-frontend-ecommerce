deb4bce9019976fa987b2f55c7238ef50c4c942a
Dashan Nadeema <dashanwd@gmail.com>
2024-04-11 13:05:32 +0530

    few changes

--- clothify/src/pages/ProductView.jsx
+++ clothify/src/pages/ProductView.jsx
@@ -24,11 +24,11 @@ const ProductView = (props) => {
             <Header />
             <div className='mt-0 h-72 relative'>
                 <img src={SummerCollectionImage} className='h-[90%] w-screen object-cover' alt='summer image' />
-                <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl tracking-widest font-bold text-center">
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl tracking-widest font-bold text-center">
                     <h1>{props.collection}</h1>
                 </div>
             </div>
-            <div class="grid grid-cols-2 max-lg:grid-cols-3 max-md:grid-cols-2 lg:grid-cols-4 gap-0 pl-10 mt-20 w-[80%] max-lg:w-[90%] max-sm:grid-cols-1 ml-2">
+            <div className="grid grid-cols-2 max-lg:grid-cols-3 max-md:grid-cols-2 lg:grid-cols-4 gap-0 pl-10 mt-20 w-[80%] max-lg:w-[90%] max-sm:grid-cols-1 ml-2">
                 {products.map((element)=>{
                     return(
                         <div>
