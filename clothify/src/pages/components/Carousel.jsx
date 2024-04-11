894fbd63c1c2a6e42c43d1146f0c4d6cb2529c26
Dashan Nadeema <dashanwd@gmail.com>
2024-04-11 12:48:57 +0530

    carousel button add !

--- clothify/src/pages/components/Carousel.jsx
+++ clothify/src/pages/components/Carousel.jsx
@@ -21,8 +21,18 @@ const Carousel = ({ children: slides,autoSlide=false, autoSlideInterval=3000 }) 
         <div className='overflow-hidden relative rounded-lg'>
             <div className='flex transition-transform ease-out duration-500'style={{transform:`translateX(-${curr*100}%)`}}>{slides}</div>
             <div className='absolute inset-0 flex items-center justify-between p-4'>
-            <button className='bg-white'onClick={prev}>Prev</button>
-            <button className='bg-white'onClick={next}>Next</button>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" onClick={prev} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+            </svg>
+            </span>
+            <span class="sr-only">Previous</span>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" onClick={next} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+            </svg>
+            </span>
+            <span class="sr-only">Next</span>
             </div>
         </div>
     )
