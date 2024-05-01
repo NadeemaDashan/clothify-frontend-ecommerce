51cf3ce216ce6d01f323c9bc975337595f74c577
Dashan Nadeema <dashanwd@gmail.com>
2024-05-01 12:36:19 +0530

    home page and header new version complete !

--- clothify/src/pages/Home.jsx
+++ clothify/src/pages/Home.jsx
@@ -22,7 +22,7 @@ import bento_m5 from '../assets/m5.png';
 import men from '../assets/mens.png';
 import season from '../assets/seasonal.png';
 import spring_m from '../assets/spring-m.png';
-import spring from '../assets/spring.png';
+import spring from '../assets/summer2.png';
 import summer_m from '../assets/summer-m.png';
 import summer from '../assets/summer.png';
 import bento1 from '../assets/T-shirtBento.png';
@@ -34,11 +34,8 @@ import women from '../assets/women.png';
 import Cart from "./components/Cart";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import ProductFilter from './components/ProductFilter';
 
 export default function Home() {
-
-    const [rotate, setRotate] = React.useState(false);
     const [isOpen, setIsOpen] = React.useState(false);
 
     function handleClick(){
@@ -48,71 +45,71 @@ export default function Home() {
     }
 
     return (
-        <div className='flex flex-col'>
-            <div>
+        <div className='flex flex-col bg-[#FCFCF5]  dark:bg-black'>
+            <div className='mb-0 p-0'>
                 <Header action={handleClick}/>
             </div>
-            <motion.main animate={{ scale: 1 }} transition={{ type: "spring", duration: 2 }} initial={{ scale: 0 }} className={`ssm:w-[80%] ssm:ml-[9%] ssm:mt-[6%] sm:w-[85%] sm:ml-[5%] sm:mt-[5%] md:w-[85%] md:ml-[6%] md:mt-[3%] lg:w-[90%] lg:ml-[5%] lg:mt-[3%] xl:w-[80%] xl:mt-[3%] xl:ml-[10%] 2xl:p-20  flex flex-col items-center justify-center  2xl:w-[90%] 2xl:ml-[4%] ${isOpen==true?` overflow-hidden`:` overflow-scroll`} `}>
+            <motion.main animate={{ scale: 1 }} transition={{ type: "spring", duration: 2 }} initial={{ scale: 0 }} className={`flex flex-col items-center justify-center 2xl:p-60 lg:p-24    ${isOpen==true?` overflow-hidden`:` overflow-scroll`} `}>
 
-                <div className="flex  justify-end ">
-                    <motion.div animate={{ rotate: rotate ? 360 : 0 }} transition={{ duration: 1 }} onClick={() => { setRotate(!rotate); }} className=" pl-12   ml-[1%] mr-[0%] pb-8  ssm:w-[118%] ssm:ml-[-12%]">
-                        <img src={start} alt="" className="" />
-                    </motion.div>
+                <div className="flex w-[100%] justify-end  overflow-y-hidden mb-12 mt-0 max-sm:p-7 max-lg:p-12 max-xl:p-16  2xl:mt-[-14%] ">
+                    <div  className=" 2xl:mt-10 pt-0 ">
+                        <img src={start} alt="" c />
+                    </div>
                     <div   className={`mt-[-4%] absolute mr-[-9%] ${isOpen==true?`fixed`:`hidden`}`}>
                         <Cart />
                     </div>
                 </div>
 
-                <div className="   ">
+                <div>
                     <img className="" src={collection} alt="" />
                 </div>
-                <div className="g grid grid-cols-2 gap-1   ml-[5%]   mt-2 p-5  ssm:hidden">
+                <div className="g grid grid-cols-2 gap-1   ml-[5%]   mt-2 p-5  max-sm:hidden">
                     <motion.img whileHover={{ scale: 1.15 }} whileInView={{ x: 0 }} initial={{ x: -20 }} className="w-[95%]" src={bento1} alt="" />
                     <motion.img whileHover={{ scale: 1.15 }} whileInView={{ x: 0 }} initial={{ x: 20 }} className=" w-[100%] mr-2 pr-6" src={bento2} alt="" />
                     <motion.img whileHover={{ scale: 1.15 }} whileInView={{ x: 0 }} initial={{ x: -20 }} className="w-[95%] pt-0 mt-[-80%] md:mt-[-75%] sm:mt-[-74%] " src={bento3} alt="" />
-                    <motion.img whileHover={{ scale: 1.15 }} whileInView={{ x: 0 }} initial={{ x: 20 }} className="pt-5 w-[97%] lg:w-[95%] md:w-[94.5%] md:h-[99%] sm:w-[93%] sm:h-[99%]  " src={bento4} alt="" />
+                    <motion.img whileHover={{ scale: 1.15 }} whileInView={{ x: 0 }} initial={{ x: 20 }} className="pt-5 w-[97%] lg:w-[95%] md:w-[94.5%] md:h-[99%] sm:w-[93%] sm:h-[99%] " src={bento4} alt="" />
                 </div>
 
-                <div className="2xl:hidden xl:hidden lg:hidden md:hidden sm:hidden ssm:mt-5 flex flex-col justify-center items-center ssm:w-[100%] ssm:mr-[4%] pl-6 ">
-                    <img className="" src={bento_m1} alt="" />
-                    <img className="ssm:mt-[-12%]" src={bento_m2} alt="" />
-                    <img className="ssm:mt-[-12%]" src={bento_m3} alt="" />
-                    <img className="ssm:mt-[-12%]" src={bento_m4} alt="" />
-                    <img className="ssm:mt-[-12%]" src={bento_m5} alt="" />
+                <div className=" flex flex-col justify-center items-center p-10 md:hidden ">
+                    <motion.img whileHover={{scale:1.15}} className="" src={bento_m1} alt="" />
+                    <motion.img whileHover={{scale:1.15}} className="ssm:mt-[-12%]" src={bento_m2} alt="" />
+                    <motion.img whileHover={{scale:1.15}} className="ssm:mt-[-12%]" src={bento_m3} alt="" />
+                    <motion.img whileHover={{scale:1.15}} className="ssm:mt-[-12%]" src={bento_m4} alt="" />
+                    <motion.img whileHover={{scale:1.15}} className="ssm:mt-[-12%]" src={bento_m5} alt="" />
                 </div>
 
-                <div className=" pl-[3%]  pr-3 flex ml-[3%] ssm:hidden">
+                <div className=" pl-[3%]  pr-3 flex ml-[3%] max-sm:hidden">
                     <motion.img whileHover={{ scale: 1.15 }} whileInView={{ x: 0 }} initial={{ x: -20 }} className="r rounded-lg w-[98%]" src={bento5} alt="" />
                 </div>
-                <div className="    mt-[10%]   ">
+                <div className="mt-[10%]">
                     <img className="ml-0 p-0  mr-[-2%]" src={category} alt="" />
                 </div>
-                <motion.div transition={{ duration: 2 }} whileInView={{ rotate: [0, 180, 180, 0] }} className="flex mt-7 ml-[2%] mr-[1%] w-[100%]  justify-center gap-5 ssm:hidden">
-                    <img className="size-[20%] " src={men} alt="" />
-                    <img className="size-[20%] object-cover" src={women} alt="" />
-                    <img className="size-[20%]" src={teen} alt="" />
-                    <img className="size-[20%]" src={kids} alt="" />
-                </motion.div>
-                <div className="mt-5 flex flex-col items-center ml-3 2xl:hidden xl:hidden lg:hidden md:hidden sm:hidden">
-                    <img src={men_mobile} alt="" />
-                    <img src={women_mobile} alt="" />
-                    <img src={teens_mobile} alt="" />
-                    <img src={kids_mobile} alt="" />
+                <div  className="flex mt-7 ml-[2%] mr-[1%] w-[100%]  justify-center gap-5 max-lg:hidden">
+                    <motion.img whileHover={{scale:1.5}} className="size-[20%] lg:size-[23%] " src={men} alt="" />
+                    <motion.img whileHover={{scale:1.5}} className="size-[20%] object-cover lg:size-[23%]" src={women} alt="" />
+                    <motion.img whileHover={{scale:1.5}} className="size-[20%] lg:size-[23%]" src={teen} alt="" />
+                    <motion.img whileHover={{scale:1.5}} className="size-[20%] lg:size-[23%]" src={kids} alt="" />
+                </div>
+                <div className="mt-5 flex flex-col items-center p-10 lg:hidden ">
+                    <motion.img whileHover={{scale:1.15}} src={men_mobile} alt="" />
+                    <motion.img whileHover={{scale:1.15}} src={women_mobile} alt="" />
+                    <motion.img whileHover={{scale:1.15}} src={teens_mobile} alt="" />
+                    <motion.img whileHover={{scale:1.15}} src={kids_mobile} alt="" />
                 </div>
                 <div className="   mt-[10%]   ">
                     <img className="ml-0 p-0  mr-[-2%]" src={season} alt="" />
                 </div>
-                <div className="grid grid-cols-2 pt-5 pl-8 gap-3 mr-2 ml-2 ssm:hidden">
+                <div className="grid grid-cols-2 pt-5 pl-8 gap-3 mr-2 ml-2 max-lg:hidden">
                     <img src={summer} alt="" />
-                    <img className="xl:w-[93%] lg:w-[92%] md:w-[85%] sm:w-[83%]" src={winter} alt="" />
-                    <img className="mt-[-14%] xl:mt-[-6%] 2xl:mt- 2xl:h-[85%] xl:h-[82.3%] lg:h-[81%] lg:mt-[-6%] md:h-[72%] md:mt-[-2%] sm:h-[80%] sm:mt-[0] " src={autumn} alt="" />
-                    <img className=" pr-12 sm:mt-[-4%] sm:h-[83%] " src={spring} alt="" />
+                    <img className="xl:w-[93%] lg:w-[92%] md:w-[85%] sm:w-[83%] mb-0" src={winter} alt="" />
+                    <img className="p-0 mt-[-6%]" src={autumn} alt="" />
+                    <img className="pr-12 h-[100%] object-cover " src={spring} alt="" />
                 </div>
-                <div className=" 2xl:hidden xl:hidden lg:hidden md:hidden sm:hidden flex flex-col items-center mt-5 pl-5 pr-3">
-                    <img className="pl-1" src={summer_m} alt="" />
-                    <img className="pl-1" src={winter_m} alt="" />
-                    <img className="ml-1" src={autumn_m} alt="" />
-                    <img className="pl-1" src={spring_m} alt="" />
+                <div className=" p-10 flex flex-col items-center lg:hidden ">
+                    <motion.img whileHover={{scale:1.15}} className="pl-1" src={summer_m} alt="" />
+                    <motion.img whileHover={{scale:1.15}} className="pl-1" src={winter_m} alt="" />
+                    <motion.img whileHover={{scale:1.15}} className="ml-1" src={autumn_m} alt="" />
+                    <motion.img whileHover={{scale:1.15}} className="pl-1" src={spring_m} alt="" />
                 </div>
                 <div className=" mt-10">
                     <img src={visit} alt="" />
--- clothify/src/pages/components/Header.jsx
+++ clothify/src/pages/components/Header.jsx
@@ -3,37 +3,40 @@ import Image from '../../../public/logo.png'
 import { Link } from 'react-router-dom'
 import CartImage from "../../assets/cart.png"
 import Menu from '../../assets/Menu.png'
+import cart from '../../assets/Light Shopping Bag.png'
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
+
 
     function handleClick() {
         setIsOpen(prevState => !prevState)
     }
     
     return (
-        <div>
-        <header className="flex sticky  bg-[#fbfcf0] font-mulish dark:bg-black">
-            <div className="w-1/3 justify-center items-center p-5 max-lg:hidden">
-                <ul className="flex gap-7 p-5 dark:text-white">
+        <div className='mt-16  w-[85%] ml-[8%]'>
+        <header className="flex sticky  bg-[#F4F4DC] font-mulish dark:bg-[#18191b]  rounded-xl h-[6rem]   ">
+            <div className="w-1/3 justify-center items-center p-7 max-lg:hidden ">
+                <ul className="flex gap-7 p-2 dark:text-white tracking-wider lg:tracking-wide lg:text-xs  text-sm">
                     <li className="ml-5 hover:underline"><Link to='/home'>Home</Link></li>
-                    <li className="hover:underline"><Link to='/mens'>Mens</Link></li>
-                    <li className="hover:underline"><Link to='/womens'>Womens</Link></li>
-                    <li className="hover:underline"><Link to='/kids'>Kids</Link></li>
-                    <li className="hover:underline"><Link to='/teens'>Teens</Link></li>
+                    <li className="hover:underline"><Link>Category</Link></li>
+                    <li className="hover:underline"><Link>SubCategory</Link></li>
+                    <li className="hover:underline"><Link>Collection</Link></li>
                 </ul>
             </div>
-            <div className="w-1/3 flex justify-center items-center max-lg:hidden p-5">
+            <div className="w-1/3 flex justify-center items-center max-lg:hidden p-5 lg:ml-10">
                 <img src={Image} className="w-40" alt="logo" />
             </div>
-            <div className="flex w-1/3 justify-end items-center p-5 gap-7 max-lg:hidden dark:text-white">
-                <img src={CartImage} className='h-6 mr-4' alt='cart' />
+            <div className="flex w-1/3 justify-end items-center p-5 gap-5 max-lg:hidden dark:text-white mr-5 text">
+                <img src={cart} className=' size-5 hidden dark:flex ' alt="" />
+                <img src={CartImage} className='h-6 mr-4 dark:hidden' alt='cart' />
                 <p className='mr-4 hover:underline'><Link to='/'>Sign in</Link></p>
+                
             </div>
-            <div className='lg:hidden p-5'>
-                <img src={Image} className='w-40' alt='logo' />
+            <div className='lg:hidden p-5  '>
+                <img src={Image} className=' size-12  w-28' alt='logo' />
             </div>
-            <div className='lg:hidden ml-auto p-5 pt-8'>
+            <div className='lg:hidden ml-auto p-5 pt-6'>
                 <img src={Menu} onClick={handleClick} className={`w-8 cursor-pointer ${isOpen===true?`hidden`:``}`} alt='menu'></img>
             </div>
         </header>
