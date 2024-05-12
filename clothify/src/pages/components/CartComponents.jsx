1cc5934150893a3b43eaef193b3f380ca1f09913
Dashan Nadeema <dashanwd@gmail.com>
2024-05-12 11:54:57 +0530

    sub and add complete for cart

--- clothify/src/pages/components/CartComponents.jsx
+++ clothify/src/pages/components/CartComponents.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from 'react-router-dom'
 
 const CartComponents = (props) => {
     const useNav=useNavigate()
-    const[qty,setQty]=useState(props.product.qty);
+   
     function handleDelete(e){
         const url=`http://localhost:8080/cart/delete/${props.id}`
         axios.delete(url).then((res)=>{
@@ -19,20 +19,28 @@ const CartComponents = (props) => {
         })
     }
     function addItems(e){
-        if(props.product.stock.qty<=qty){
-            alert("You can't add more items sorry stock is not available to facilitate the required need")
+        const url=`http://localhost:8080/cart/update/add/${props.id}`
+        if(props.product.stock.qty<=props.product.qty){
+            alert("Out of Stock")
         }else{
-            setQty(qty=>qty+1);
-
+        axios.put(url).then((res)=>{
+            console.log(res.data)
+            props.reload();
+        })
+    }
+    }
+    function subItems(e){
+        const url=`http://localhost:8080/cart/update/sub/${props.id}`
+        if(props.product.qty>1){
+        axios.put(url).then((res)=>{
+            console.log(res.data)
+            props.reload();
+        })
+        }else{
+            alert("if you want to remove click the bin icon");
         }
     }
-    function removeItems(e){
-        if(qty>1){
-            setQty(qty=>qty-1)
-        }else{
-            alert("You can't remove more items sorry if you want to remove click delete icon")
-        }
-    }
+    
     return (
         <div className='flex mb-5 '>
             <div className=' ml-4 w-[30%]'>
@@ -45,8 +53,8 @@ const CartComponents = (props) => {
                 </div>
                 <span className='ml-[10%] mt-3'>{`${props.product.stock.size} / ${props.product.stock.color}`}</span>
                 <div className='flex flex-row size-[15%] ml-[10%] mt-4'>
-                    <img src={min} alt="reduce button" onClick={(e)=>removeItems(e)} className='w-6 object-cover' />
-                    <p className='px-10 mx-2 bg-black rounded-sm text-white pb-6'>{qty}</p>
+                    <img src={min} alt="reduce button" onClick={(e)=>subItems(e)} className='w-6 object-cover' />
+                    <p className='px-10 mx-2 bg-black rounded-sm text-white pb-6'>{props.product.qty}</p>
                     <img src={max} onClick={(e)=>addItems(e)} alt='add more button'className='w-6 object-cover' />
                     <img className='h-7 mx-3' src={bin} onClick={(e)=>handleDelete(e)} alt='bin'/>
                 </div>
